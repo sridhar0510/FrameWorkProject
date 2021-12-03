920a0d6ed7c28a3f168a0b563bb9667aefb397fa
sridhar0510 <karthisridhar135@gmail.com>
2021-12-03 08:27:39 +0530

    Recently updated

--- src/test/java/org/test/Parametres.java
+++ src/test/java/org/test/Parametres.java
@@ -1,14 +1,27 @@
 package org.test;
 
+import java.io.IOException;
+
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.utilities.BaseClass;
+import org.utilities.FbLoginPojoPage;
 
-public class Parametres {
-	
+public class Parametres extends BaseClass{
+	@Parameters({"Greens", "Greens@123"})
 	@Test
-	private void tc1() {
+	  private void tc1(String s1,String s2) throws IOException {
 		
-		System.out.println("testcase 1");
-
+		launchChrome();
+		winMax();
+		loadUrl("https://en-gb.facebook.com/");
+		FbLoginPojoPage f=new FbLoginPojoPage();
+		fill(f.getTxtuser(), s1);
+	    
+		fill(f.getTxtpass(), s2);
+	
+		button(f.getBtnLogin());
 	}
+	
 
 }
--- src/test/java/org/test/TestNg.java
+++ src/test/java/org/test/TestNg.java
@@ -1,9 +1,16 @@
 package org.test;
 
-import java.io.IOException;
+import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.util.Date;
+
+import org.junit.Ignore;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.utilities.BaseClass;
 import org.utilities.FbLoginPojoPage;
@@ -16,11 +23,10 @@ public class TestNg extends BaseClass{
 		launchChrome();
 		winMax();
 		loadUrl("https://en-gb.facebook.com/");
-		WebElement f = driver.findElement(By.id("email"));
-		f.sendKeys("8892429566");
-		WebElement d = driver.findElement(By.name("pass"));
-		d.sendKeys("Krishnamoorthy@123");
-
+		FbLoginPojoPage f=new FbLoginPojoPage();
+		fill(f.getTxtuser(), getData(1, 0));
+	    
+		fill(f.getTxtpass(), getData(2, 0));
 	}
 	
 	@Test(priority=5)
@@ -29,5 +35,49 @@ public class TestNg extends BaseClass{
 		FbLoginPojoPage f=new FbLoginPojoPage();
 		button(f.getBtnLogin());
 	}
+	
+	
+	@Test (enabled=false)
+	private void tc3() {
+		System.out.println("Test Case 3");
 
+	}
+	 @Test(priority=6)
+	private void tc4() {
+		System.out.println("Test case 4");
+
+	}
+	 @BeforeClass
+	 private void startTime() {
+		Date d=new Date();
+		System.out.println(d);
+	}
+   
+	 @AfterClass
+	 private void endTime() {
+		
+		 Date d =new Date();
+		 System.out.println(d);
+
+	}
+	 
+	 @Test
+	 private void tc5() {
+		String title = driver.getTitle();
+		Assert.assertTrue(title.contains("Facebook"), "Invalid Title");
+
+	}
+	 
+	 @Test(priority=7)
+	 private void tc6() {
+		System.out.println("Success");
+
+	}
+	 
+	 @Test
+	 private void tc7() {
+		String currentUrl = driver.getCurrentUrl();
+		Assert.assertEquals("https://en-gb.facebook.com/", "https://en-gb.facebook.com", "Current URl failed");
+
+	}
 }
